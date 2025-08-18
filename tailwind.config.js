6ca9e54abf4a4a7d0c6df6bdf9d390b1953a0e80
dronedadmedia <josh@dronedadmedia.com>
2025-08-18 14:44:32 -0500

    Tailwind CLI completed

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html", // Your HTML file
+    // Add any other HTML files here
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
